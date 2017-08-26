088b6a954d043bed5e10586a857d6196dba7e2f6
LIUXIN <liuxinat126@126.com>
2017-08-26 18:59:01 +0800

    8.26

--- www/js/controllers/top.ctrl.js
+++ www/js/controllers/top.ctrl.js
@@ -5,6 +5,7 @@
 angular.module('strawberry.top.ctrl', [])
   .controller('topCtrl',['$scope','$ionicHistory',
     function($scope,$ionicHistory){
+
       var init=function(){
         $scope.currentCtrl="topCtrl";
         $scope.currentState=$ionicHistory.currentStateName();
--- www/js/directives.js
+++ www/js/directives.js
@@ -2,24 +2,35 @@
  * Created by LIUXIN on 2017/8/16.
  */
 angular.module('starter.directives',[])
-.directive("mySubHeader",['$scope','$rootScope',function ($scope,$rootScope) {
+.directive("mySubHeader",function () {
   return {
     restrict:'EA',
     template:'<div class="sub-header">' +
     '<div class="btns">' +
-    '<div class="icon ion-ios-arrow-back" ng-click="back()"></div>' +
+    '<div class="icon ion-ios-arrow-back" ng-click="leftBtnClick()"></div>' +
     '</div>' +
-    '<div class="title">{{title}}</div>' +
+    '<div class="title">{{title}}{{msg}}</div>' +
     '<div class="btns">' +
-    '<div class="icon ion-ios-more" style="font-size:24px;"></div>' +
+    '<div class="icon ion-ios-more" style="font-size:24px;" ng-click="rightBtnClick()"></div>' +
     '</div>' +
     '</div>',
     replace:true,
     scope:{
-      title:'@title'
-    }
+      title:'@title',
+      leftBtn:'&',
+      rightBtn:'&'
+    },
+    controller:
+      function($scope) {
+        $scope.leftBtnClick=function(){
+          $scope.leftBtn();
+        }
+        $scope.rightBtnClick=function(){
+          $scope.rightBtn();
+        }
+      }
   }
-}]);
+});
 
 
 
