e7592d24be8bffa1cf7ac842fd2400c11cae76ca
liuxin <liuxinat126@126.com>
2017-09-08 17:34:13 +0800

    9.8

--- www/js/controllers/chapter.ctrl.js
+++ www/js/controllers/chapter.ctrl.js
@@ -2,8 +2,8 @@
  * Created by LIUXIN on 2017/8/14.
  */
 angular.module('strawberry.chapter.ctrl', ['starter.services'])
-  .controller('chapterCtrl', ['$scope', '$$strawberry', '$stateParams', '$state',
-    function ($scope, $$strawberry, $stateParams, $state) {
+  .controller('chapterCtrl', ['$scope', '$$strawberry', '$stateParams', '$state','$ionicSlideBoxDelegate',
+    function ($scope, $$strawberry, $stateParams, $state,$ionicSlideBoxDelegate) {
 
       var bookid = $stateParams.id;
 
@@ -25,16 +25,24 @@ angular.module('strawberry.chapter.ctrl', ['starter.services'])
       }
 
       function chapterContent(chapter_index) {
-        $$strawberry.getReadChapter({id: bookid, index: chapter_index}, {
+        var index=parseInt(chapter_index)+1;
+        $$strawberry.getReadChapter({id: bookid, index: index}, {
           onSuccess: function (data) {
+            $ionicSlideBoxDelegate.update();
             if (!data.error) {
-              console.log("获取到第" + chapter_index + "章节内容成功", data.result);
+              console.log("获取到第" + index + "章节内容成功", data.result);
               $scope.chapterContent = data.result;
+              //处理 拿到的文章内容
+
+
+
+
+
               //分段
               $scope.paragraphs=$scope.chapterContent.content.split(/\s+/);
-
+              console.log($scope.paragraphs);
             }else{
-              console.log("获取到第" + chapter_index + "章节内容失败", data.error);
+              console.log("获取到第" + index + "章节内容失败", data.error);
             }
           },
           onError: function () {
@@ -44,12 +52,12 @@ angular.module('strawberry.chapter.ctrl', ['starter.services'])
 
 
       //分页
-
-      function page(){
-        var page_height=$rootScope.screenHeight_total-60-30;
-        var line_count = page_height/24;
-        var height = document.getElementByClassName('content')[0]
-      }
+      //
+      //function page(){
+      //  var page_height=$rootScope.screenHeight_total-60-30;
+      //  var line_count = page_height/24;
+      //  var height = document.getElementByClassName('content')[0]
+      //}
       var init = function () {
         if ($state.current.name == 'chapter.menu') {
           getNovelDetail();
